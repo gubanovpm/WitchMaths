9d8d27beb175862e0a7a09928a979f8ad78a341c
gubanovpm <petrgubanov37@gmail.com>
2022-11-07 20:16:29 +0300

    all

--- 1_lab/third_task/srcs/main.cc
+++ 1_lab/third_task/srcs/main.cc
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
 #include <matplot/matplot.h>
 
 using namespace boost::numeric::ublas;
 
-#define EPSILON 1e-6
+#define EPSILON 1e-3
+#define EPS 0
 
 int main() {
     //========================================================================================
@@ -17,7 +19,7 @@ int main() {
     matrix<double> M  = {3, 1}; // кг/моль
     M(0, 0) = 0.028013, M(1, 0) = 0.04401, M(2, 0) = 0.03007;
     matrix<double> Pc = {3, 1}; // Па
-    Pc(0, 0) = 0.00033944, Pc(1, 0) = 0.00073866, Pc(2, 0) = 0.00048839;
+    Pc(0, 0) = 339440, Pc(1, 0) = 738660, Pc(2, 0) = 488390;
     matrix<double> Tc = {3, 1}; // К
     Tc(0, 0) = 126.2, Tc(1, 0) = 304.7, Tc(2, 0) = 305.43;
 
@@ -39,10 +41,10 @@ int main() {
     // Пользовательские данные (вторая размерность =2 так как по заданию для двух пар (T, P))
     //========================================================================================
     matrix<double> T = {3, 1}; // К
-    T(0, 0) = 73, T(1, 0) = 73, T(2, 0) = 73;
+    T(0, 0) = 200, T(1, 0) = 200, T(2, 0) = 200;
 
     matrix<double> P = {3, 1}; // Па
-    P(0, 0) = 0.0001, P(1, 0) = 0.0001, P(2, 0) = 0.0001;
+    P(0, 0) = 200000, P(1, 0) = 200000, P(2, 0) = 200000;
     //==============================================================================================================================================
     // Вспомогательные лямбда-функции
     //==============================================================================================================================================
@@ -69,7 +71,7 @@ int main() {
         }
         return sum;
     };
-    auto get_s_i = [](const matrix<double> &a, const matrix<double> &c, const int i) {
+    auto get_s_i = [](const matrix<double> &a, const matrix<double> &c, const size_t i) {
         double sum = 0;
         for (size_t k = 0; k < c.size1(); ++k) {
             sum += a(i, k) * c(k, 0);
@@ -77,23 +79,26 @@ int main() {
         return sum;
     };
     auto get_f_i = [](const double _p, const double _T, const double _c_i, const double _Z, const double _a, const double _b, const double _s_i, const double _a_i, const double _b_i) {
-        return std::log(_p * _c_i) + std::log(_Z - _b) + _a / (2 * std::sqrt(2) * _b) * (2*_s_i/_a - _b_i/_b) * std::log((_Z - (std::sqrt(2) - 1) * _b) / (_Z + (std::sqrt(2) + 1) * _b)) + _b_i/_b * (_Z - 1);
+        return std::log(_p * _c_i) - std::log(_Z - _b) + _a / (2 * std::sqrt(2) * _b) * (2*_s_i/_a - _b_i/_b) * std::log((_Z - (std::sqrt(2) - 1) * _b) / (_Z + (std::sqrt(2) + 1) * _b)) + _b_i/_b * (_Z - 1);
     };
 
     //==============================================================================================================================================
     // Основная функция
     //==============================================================================================================================================
-    std::vector<double> phi = {0.25, 0.5, 0.75};
+    std::vector<double> phi = {0.75};
+    // size_t count = 100;
+    // for (size_t i = 0 ; i < count; ++i) phi.push_back(double(i)/count);
+
     std::vector<double> x = {};
     std::vector<size_t> y = {};
     
     for (size_t i = 0; i < phi.size(); ++i) {
         double PHI = phi[i];
-        for (size_t pairs_count = 0; pairs_count < std::min(T.size1(), P.size1()); ++pairs_count) {
+        for (size_t pairs_count = 0 ; pairs_count < std::min(T.size1(), P.size1()); ++pairs_count) {
             // Сбросим значения для первой итерации
             z(0, 0) = 0.0189, z(1, 0) = 0.1567, z(2, 0) = 0.8244;
             size_t iteration = 0;
-            matrix<double> c_g = {3, 1}, c_l = {3, 1};
+            matrix<double> c_g = {3, 1},  c_l = {3, 1};
             matrix<double> c_g_star = {3, 1}, c_l_star = {3, 1};
             matrix<double> K  = {3, 1};
             matrix<double> Tr = {3, 1};
@@ -104,7 +109,7 @@ int main() {
             matrix<double> b_i  = {3, 1};
             
             matrix<double> a_ij = {3, 3};
-            matrix<double> s_i = {3, 1};
+            matrix<double> s_l_i = {3, 1}, s_g_i = {3, 1};
             
             // Вызов лямбда функций для заполнения соответствующих векторов
             for (size_t i = 0; i < 3; ++i) {
@@ -133,53 +138,137 @@ int main() {
                 double D = std::pow(b_koef, 2) - 4 * a_koef * c_koef;
                 // std::cout << "Корни по альфа: " <<  (-b_koef - std::sqrt(D)) / (2 * a_koef) << " " << (-b_koef + std::sqrt(D)) / (2 * a_koef) << std::endl;
 
-                // I don't know how we are must calc two roots of equation
-                double Z_l = 0.01, Z_g = 0.01;
-
                 double alpha = (-b_koef + std::sqrt(D)) / (2 * a_koef);
                 // here's strange partition of code
                 c_g(0, 0) = z(0, 0) * K(0, 0) / (alpha * (K(0,0) - 1) + 1);
                 c_l(0, 0) = z(0, 0) / (alpha * (K(0,0) - 1) + 1);
                 
                 for (size_t i = 1; i < 3; ++i) {
-                    c_g_star(i, 0) = z(i, 0) * K(i, 0) / ( lambdas(i, 0) * (K(i, 0) - 1) + 1);
+                    c_g_star(i, 0) = z(i, 0) * K(i, 0) / ( alpha * (K(i, 0) - 1) + 1);
                     c_g(i , 0) = PHI * c_g_star(i, 0) + (1 - PHI) * c_g(i - 1, 0);
 
-                    c_l_star(i, 0) = z(i, 0) / ( lambdas(i, 0) * (K(i, 0) - 1) + 1);
+                    c_l_star(i, 0) = z(i, 0) / ( alpha * (K(i, 0) - 1) + 1);
                     c_l(i , 0) = PHI * c_l_star(i, 0) + (1 - PHI) * c_l(i - 1, 0);
                 }
 
                 double a_l = get_a(a_ij, c_l), a_g = get_a(a_ij, c_g);
                 double b_l = get_b(b_i , c_l), b_g = get_b(b_i , c_g);
 
-                matrix<double> f_l = {3, 1}, f_g = {3, 1};
                 for (size_t i = 0; i < 3; ++i) {
-                    f_l(i, 0) = get_f_i(P(pairs_count, 0), T(pairs_count, 0), c_l(i, 0), Z_l, a_l, b_l, s_i(i, 0), a_i(i, 0), b_i(i, 0));
-                    f_g(i, 0) = get_f_i(P(pairs_count, 0), T(pairs_count, 0), c_g(i, 0), Z_g, a_g, b_g, s_i(i, 0), a_i(i, 0), b_i(i, 0));
-                    K(i, 0) *= (f_l(i, 0) / f_g(i, 0));
+                    s_l_i(i, 0) = get_s_i(a_ij, c_l, i);
+                    s_g_i(i, 0) = get_s_i(a_ij, c_g, i);
                 }
 
-                std::cout << f_l << std::endl;
-                std::cout << f_g << std::endl;
+                // Решим кубическое уравнение тригонометрическим способом
+                double a_c_l = b_l - 1, b_c_l = a_l - 2 * b_l - 3 * std::pow(b_l, 2), c_c_l = std::pow(b_l, 3) + std::pow(b_l, 2) - a_l * b_l;
+                double Q_l = -(3*b_c_l - std::pow(a_c_l, 2)) / 9;
+                double R_l = -(9*a_c_l*b_c_l - 2 * std::pow(a_c_l, 3) - 27 * c_c_l) / 54;
+                double S_l = std::pow(Q_l,  3) - std::pow(R_l, 2);
+
+                double a_c_g = b_g - 1, b_c_g = a_g - 2 * b_g - 3 * std::pow(b_g, 2), c_c_g = std::pow(b_g, 3) + std::pow(b_g, 2) - a_g * b_g;
+                double Q_g = -(3*b_c_g - std::pow(a_c_g, 2)) / 9;
+                double R_g = -(9*a_c_g*b_c_g - 2 * std::pow(a_c_g, 3) - 27 * c_c_g) / 54;
+                double S_g = std::pow(Q_g,  3) - std::pow(R_g, 2);
+
+                double psi_l = 0, psi_g = 0;
+                double t_1 = 0, t_2 = 0;
+                double x_1, x_2, x_3;
+                std::vector<double> x = {};
+
+                // std::cout << "x^3 + " <<  a_c_l << " x^2 + " << b_c_l << " x + " << c_c_l << std::endl;
+                // std::cout << "x^3 + " <<  a_c_g << " x^2 + " << b_c_g << " x + " << c_c_g << std::endl; 
+
+                if (S_l > EPS) {
+                    psi_l = 1./3 * std::acos(R_l / std::sqrt(std::pow(Q_l, 3)));
+                    std::cout << "psi L " <<-2. * std::sqrt(Q_l) * std::cos(psi_l) << std::endl;
+                    x_1 = -2. * std::sqrt(Q_l) * std::cos(psi_l) - a_c_l / 3.;
+                    x_2 = -2. * std::sqrt(Q_l) * std::cos(psi_l + M_PI * 2./3) - a_c_l / 3.;
+                    x_3 = -2. * std::sqrt(Q_l) * std::cos(psi_l - M_PI * 2./3) - a_c_l / 3.;
+                    if (x_1 > 0) x.push_back(x_1);
+                    if (x_2 > 0) x.push_back(x_2);
+                    if (x_3 > 0) x.push_back(x_3);
+
+                    std::cout << x_1 << " ; " << x_2 << " ; " << x_3 << std::endl;
+                } else if (S_l < -EPS) {
+                    if (Q_l > EPS) {
+                        psi_l = 1./3 * std::acosh(std::abs(R_l) / std::sqrt(std::abs(std::pow(Q_l, 3))));
+                        double siln_R_l = (R_l == 0 ? 0. : R_l / std::abs(R_l));
+                        x_2 = -2. * siln_R_l * std::sqrt(std::abs(Q_l)) * std::cosh(psi_l) - a_c_l / 3;
+
+                        if (x_2 > 0) x.push_back(x_2);
+                    }
+                    else {
+                        psi_l = 1./3 * std::asinh(std::abs(R_l) / std::sqrt(std::abs(std::pow(Q_l, 3))));
+                        double siln_R_l = (R_l == 0 ? 0. : R_l / std::abs(R_l));
+                        x_2 = -2. * siln_R_l * std::sqrt(std::abs(Q_l)) * std::sinh(psi_l) - a_c_l / 3;
+
+                        if (x_2 > 0) x.push_back(x_2);
+                    }                  
+                }
+
+                // x = {};
+                if (S_g > EPS) {
+                    psi_g = 1./3 * std::acos(R_g / std::sqrt(std::pow(Q_g, 3)));
+                    x_1 = -2 * std::sqrt(Q_g) * std::cos(psi_g) - a_c_g/3;
+                    x_2 = -2 * std::sqrt(Q_g) * std::cos(psi_g + M_PI * 2/3) - a_c_g / 3;
+                    x_3 = -2 * std::sqrt(Q_g) * std::cos(psi_g - M_PI * 2/3) - a_c_g / 3;
+                    if (x_1 > 0) x.push_back(x_1);
+                    if (x_2 > 0) x.push_back(x_2);
+                    if (x_3 > 0) x.push_back(x_3);
+
+                } else if (S_g < -EPS) {
+                    if (Q_g > EPS) {
+                        psi_g = 1./3 * std::acosh(std::abs(R_g) / std::sqrt(std::abs(std::pow(Q_g, 3))));
+                        double sign_R_g = (R_g == 0 ? 0. : R_g / std::abs(R_g));
+                        x_2 = -2. * sign_R_g * std::sqrt(std::abs(Q_g)) * std::cosh(psi_g) - a_c_g / 3;
+
+                        if (x_2 > 0) x.push_back(x_2);
+                    }
+                    else {
+                        psi_g = 1./3 * std::asinh(std::abs(R_g) / std::sqrt(std::abs(std::pow(Q_g, 3))));
+                        double sign_R_g = (R_g == 0 ? 0. : R_g / std::abs(R_g));
+                        x_2 = -2. * sign_R_g * std::sqrt(std::abs(Q_g)) * std::sinh(psi_g) - a_c_g / 3;
+
+                        if (x_2 > 0) x.push_back(x_2);
+                    }
+                }
+
+                if (x.size() > 0)
+                    t_1 = *std::min_element(x.begin(), x.end());
+                if (x.size() > 0)
+                    t_2 = *std::max_element(x.begin(), x.end());
+
+                std::cout << Q_l << " ; " << R_l << " ; "  << S_l << " >> " << psi_l << " " << t_1 << std::endl;
+                // std::cout << Q_g << " ; " << R_g << " ; "  << S_g << " >> " << psi_g << " " << t_2 << std::endl;
+
+                matrix<double> f_l = {3, 1}, f_g = {3, 1};
+                for (size_t i = 0; i < 3; ++i) {
+                    f_l(i, 0) = get_f_i(P(pairs_count, 0), T(pairs_count, 0), c_l(i, 0), t_1, a_l, b_l, s_l_i(i, 0), a_i(i, 0), b_i(i, 0));
+                    f_g(i, 0) = get_f_i(P(pairs_count, 0), T(pairs_count, 0), c_g(i, 0), t_2, a_g, b_g, s_g_i(i, 0), a_i(i, 0), b_i(i, 0));
+                    K(i, 0) *= (f_l(i, 0) / f_g(i, 0));
+                    std::cout << f_l << " ; " << f_g << std::endl;
+                }
 
                 bool flag = true;
                 for (size_t i = 0; i < 3; ++i) {
-                    flag = (std::abs(f_l(i, 0) / f_g(i, 0) - 1) < EPSILON);
+                    flag &= (std::abs(f_l(i, 0) / f_g(i, 0) - 1) < EPSILON);
                     if (!flag) break;
                 }
 
-                if (flag || (iteration == 10000)) break;
+                if (flag || (iteration == 1000)) break;
                 ++iteration;
+
+                break;
             }
             x.push_back(PHI);
             y.push_back(iteration);
         }
     }
 
-    matplot::plot(x, y)->line_width(1).color("red");
-    matplot::xlabel("Параметр Фи");
-    matplot::ylabel("Количество итераций");
-    matplot::show();
+    // matplot::plot(x, y)->line_width(1).color("red");
+    // matplot::xlabel("Параметр Фи");
+    // matplot::ylabel("Количество итераций");
+    // matplot::show();
     
     return 0;
 }

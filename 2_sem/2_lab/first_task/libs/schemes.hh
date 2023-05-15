51277edfc35e7e33bb959755f0fbc10929602b5e
gubanovpm <petrgubanov37@gmail.com>
2023-05-16 00:45:02 +0300

    for swap system

--- 2_sem/2_lab/first_task/libs/schemes.hh
+++ 2_sem/2_lab/first_task/libs/schemes.hh
@@ -3,55 +3,39 @@
 
 #include <iostream>
 #include <matplot/matplot.h>
-#include <pthread.h>
-
-#ifndef THREAD_COUNT
-#define THREAD_COUNT 6
-#endif
 
 using utype = std::function<double(const double)>;
 
 namespace WitchMath {
 
-enum SchemeT {
-	Lux = 0, 
-	LuxWendorf, 
-	RightCorner, 
-	LeftCorner
-};
-
 struct matrix_t final{
 private:
-	size_t __n = 0;
-	size_t __m = 0;
-	double *__data = nullptr;
-	utype __u;
+	size_t n = 0;
+	size_t m = 0;
+	double X = 0.;
+	double T = 0.;
+	double x = 0.;
+	double t = 0.;
+	double *data = nullptr;
+	utype u;
+	size_t type = 0;
 
-	double Lux_scheme(const size_t i, const size_t j) const noexcept;
-	double Lux_Wendorf_scheme(const size_t i, const size_t j) const noexcept;
-	double right_corner_scheme(const size_t i, const size_t j) const noexcept;
-	double left_corner_scheme(const size_t i, const size_t j) const noexcept;
+	double Lux_scheme(const size_t i, const size_t j, const double c) const;
+	double Lux_Wendorf_scheme(const size_t i, const size_t j) const;
+	double right_corner_scheme(const size_t i, const size_t j) const;
+	double left_corner_scheme(const size_t i, const size_t j) const;
 public:
-	matrix_t(const size_t n, const size_t m, const utype u) :
-		__n(n), __m(m), __data(new double [n * m]), __u(u) {}
+	matrix_t(const double X, const double T,const size_t n, const size_t m, const size_t type, const utype u) :
+		n(n), m(m), X(X), T(T), x((double)X/m), t((double)T/n), data(new double [n * m]), u(u), type(type) {}
 
-	double &operator()(const size_t i, const size_t j) const noexcept { return __data[i*__m + j]; }	
-	size_t getn() const noexcept { return __n; }
-	size_t getm() const noexcept { return __m; }
-	double u(const double x) const {return __u(x);}
+	double &operator()(const size_t i, const size_t j) const noexcept { return data[i*m + j]; }	
+	void compute();
+	void save_img(const std::string);
 
-	~matrix_t() { delete [] __data; }
+	~matrix_t() { delete [] data; }
 };
 
-struct thread_arg_t final {
-	size_t id;
-	size_t np;
-	size_t tp;
-	matrix_t *matrix;
-};
-
-void *compute(void *thread_args);
-
+void get_args(const int argc, const char *argv[], matrix_t **matrix, const utype u) noexcept;
 };
 
 #endif
--- 2_sem/2_lab/first_task/srcs/schemes.cc
+++ 2_sem/2_lab/first_task/srcs/schemes.cc
@@ -2,7 +2,57 @@
 
 using namespace WitchMath;
 
-void *WitchMath::compute(void *thread_args) {
-	printf("Hello\n");
-	return nullptr;
+void WitchMath::get_args(const int argc, const char *argv[], matrix_t **matrix, const utype u) noexcept {
+	if (argc != 6) {
+		std::cout << "Usage is: " << argv[0] << " <X> <T> <m> <k> <type>" << std::endl;
+		std::cout << "<X> -- max X value (min X = 0)" << std::endl;
+		std::cout << "<T> -- max T value (min T = 0)" << std::endl;
+		std::cout << "<m> -- count of steps on X coord" << std::endl;
+		std::cout << "<k> -- count of steps on T coord" << std::endl;
+		std::cout << "<type> -- using scheme type:" << std::endl;
+		std::cout << "\t0 - Lux scheme" << std::endl;
+		std::cout << "\t1 - Lux-Wendorf scheme" << std::endl;
+		std::cout << "\t2 - Right corner scheme" << std::endl;
+		std::cout << "\t3 - Left corner scheme" << std::endl;
+		exit(-1);
+	}
+	*matrix = new matrix_t {std::atof(argv[1]), std::atof(argv[2]), std::atoi(argv[3]), std::atoi(argv[4]), std::atoi(argv[5]), u};	
+}
+
+void matrix_t::compute() {	
+	double h = x;
+	for (size_t j = 0; j < m; ++j) {	
+		data[j] = u(j*h);
+	}
+	switch (type) {
+		case 0: { //Lux scheme
+			printf("starting computing Lux scheme, with t = %lg, h = %lg\n", t, h);
+			for (size_t i = 1; i < n; ++i)
+				for (size_t j = 0; j < m; ++j) {
+					Lux_scheme(i-1, j, data[(i-1)*m + j]);
+				}
+		}
+	}
+}
+
+double matrix_t::Lux_scheme(const size_t i, const size_t j, const double c) const {
+	double h = x;
+	if (j   == 0) return (data[i*m+j+1]*(h-c*t) + data[i*m+  j]*(h+c*t)) / (2*h);
+	if (j+1 == m) return (data[i*m+  j]*(h-c*t) + data[i*m+j-1]*(h+c*t)) / (2*h);
+	return               (data[i*m+j+1]*(h-c*t) + data[i*m+j-1]*(h+c*t)) / (2*h);
+}
+
+void matrix_t::save_img(const std::string path) {
+	auto f = matplot::figure(true);
+	auto [R, Y] = matplot::meshgrid(matplot::linspace(0, T, n), matplot::linspace(0, X, m));
+	auto [I, J] = matplot::meshgrid(matplot::linspace(0, n-1, n), matplot::linspace(0, m-1, m));
+	auto Z = matplot::transform(I, J, [this](size_t i, size_t j) {  
+		return data[i*m + j];
+	});
+
+	matplot::surf(Y, R, Z);
+	matplot::xlabel("X");
+	matplot::ylabel("T");
+	//matplot::save(path);
+	matplot::show();
 }

c5329ac3d2a789d7112aaa0c81e528197f07550c
gubanovpm <petrgubanov37@gmail.com>
2023-05-17 06:27:28 +0300

    some changes

--- 2_sem/coursework/srcs/shmemes.cc
+++ 2_sem/coursework/srcs/shmemes.cc
@@ -8,8 +8,8 @@ void WitchMath::get_args(const int argc, const char *argv[], matrix_t **matrix) 
 		std::cout << "Usage is: " << argv[0] << " <Tmax> <Rmax> <k> <m>" << std::endl;
 		std::cout << "<Tmax> -- max Tmax value (Tmin = 0 by default)" << std::endl;
 		std::cout << "<Rmax> -- max Rmax value (Rmin = 0 by default)" << std::endl;
-		std::cout << "<k>    -- count of steps on X coord" << std::endl;
-		std::cout << "<m>    -- count of steps on T coord" << std::endl;
+		std::cout << "<k>    -- count of steps on r coord" << std::endl;
+		std::cout << "<m>    -- count of steps on t coord" << std::endl;
 		exit(-1);
 	}
 	*matrix = new matrix_t {
@@ -34,7 +34,11 @@ void matrix_t::scheme_u (const size_t i, const size_t j, const double c) noexcep
 }
 void matrix_t::scheme_u (const size_t i, const double c) noexcept {
     double k = c * tau / h;
-    u[i+1][0] = k/2 * (k - 1) * P[i][1]+ u[i][0];
+
+    u[i+1][0] = k/2 * (
+        (k - 1) * P[i][1] -
+        2 * P[i][0]
+    ) + u[i][0];
 }
 //===============================================================================================
 //===============================================================================================
@@ -42,6 +46,7 @@ void matrix_t::scheme_ro(const size_t i, const size_t j, const double c) noexcep
     double k = c * tau / h;
     size_t jmn = ((j  == 0) ? j : j-1);
     size_t jmx = ((j+1== m) ? j : j+1);
+
     ro[i+1][j] = k/2 * (
         (k - 1) * std::pow(jmx*h, 2) * ro[i][jmx] * u[i][jmx] - 
         2 * std::pow(j*h, 2) * ro[i][j] * u[i][j] +
@@ -50,7 +55,11 @@ void matrix_t::scheme_ro(const size_t i, const size_t j, const double c) noexcep
 }
 void matrix_t::scheme_ro(const size_t i, const double c) noexcept {
     double k = c * tau / h;
-    ro[i+1][0] = k/2 * (k - 1) * std::pow(h, 2) * ro[i][1] * u[i][1] + ro[i][0];
+
+    ro[i+1][0] = k/2 * (
+        (k - 1) * std::pow(h, 2) * ro[i][1] * u[i][1] -
+        2 * std::pow(h, 2) * ro[i][0] * u[i][0]
+    ) + ro[i][0];
 }
 //===============================================================================================
 //===============================================================================================
@@ -64,11 +73,17 @@ void matrix_t::scheme_EP(const size_t i, const size_t j, const double c) noexcep
         2 * std::pow(j*h, 2) * u[i][j] +
         (k + 1) * std::pow(jmn*h, 2) * u[i][jmn]
     ) + E[i][j];
+
     P[i+1][j] = E[i+1][j] * ro[i+1][j] * (g - 1);
 }
 void matrix_t::scheme_EP(const size_t i, const double c) noexcept {
     double k = c * tau / h, result = 0;
-    E[i+1][0] = k/2 * (k - 1) * std::pow(h, 2) * u[i][1] + E[i][0];
+    
+    E[i+1][0] = k/2 * (
+        (k - 1) * std::pow(h, 2) * u[i][1] - 
+        2 * std::pow(h, 2) * u[i][0]
+    ) + E[i][0];
+
     P[i+1][0] = E[i+1][0] * ro[i+1][0] * (g - 1);
 }
 //===============================================================================================
@@ -92,11 +107,12 @@ void matrix_t::compute() {
 //===============================================================================================
 void matrix_t::view_plot(const size_t plot_num) noexcept {
     std::vector<std::vector<double>> *data = nullptr;
+    std::string name;
     switch (plot_num) {
-        case 0: { data = &ro; break;}
-        case 1: { data =  &u; break;}
-        case 2: { data =  &E; break;}
-        case 3: { data =  &P; break;}
+        case 0: { data = &ro; name = "ro(t, r)"; break;}
+        case 1: { data =  &u; name = "u(t, r)"; break;}
+        case 2: { data =  &E; name = "E(t, r)"; break;}
+        case 3: { data =  &P; name = "P(t, r)"; break;}
         default: {
             std::cout << "Plot types is: " << std::endl;
             std::cout << "\t 0 -- ro(t, r)" << std::endl;
@@ -121,10 +137,15 @@ void matrix_t::view_plot(const size_t plot_num) noexcept {
 		return (*data)[i][j];
 	});
 
-	matplot::contour(Y, R, Z);
+#ifdef DEBUG
     matplot::surf(Y, R, Z);
+#else
+    matplot::contourf(Y, R, Z);
+#endif
+
 	matplot::xlabel("r");
 	matplot::ylabel("t");
+    matplot::title(name);
 	//matplot::save(path);
 	matplot::show();
 }
